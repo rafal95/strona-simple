aa58b5ee0c39b68a07aef7f4485ffbced113cac2
rafal95 <47984361+rafal95@users.noreply.github.com>
2019-03-11 13:32:34 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function searchClick(){
+		if(document.getElementById('id').style.display=='block')
+			document.getElementById('id').style.display='none';
+		else
+			document.getElementById('id').style.display='block';
+}
